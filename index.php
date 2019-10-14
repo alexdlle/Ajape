7db3795f5762c087beed968666dadd2faf841127
Hugo <h.deschamps@stjosup.com>
2019-10-14 11:38:19 -0700

    fdfd

--- index.php
+++ index.php
@@ -5,25 +5,25 @@ ob_start();
 
 ?>
 
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div id="carouselInterval" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
+    <div class="carousel-item active" data-interval="10000">
+      <img src="https://blog.sportacus.fr/wp-content/uploads/2017/02/Capture-d%E2%80%99e%CC%81cran-2017-02-15-a%CC%80-22.04.47.png" class="d-block w-100" alt="">
+    </div>
+    <div class="carousel-item" data-interval="2000">
+      <img src="https://blog.sportacus.fr/wp-content/uploads/2017/02/Capture-d%E2%80%99e%CC%81cran-2017-02-15-a%CC%80-22.04.47.png" class="d-block w-100" alt="">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="https://blog.sportacus.fr/wp-content/uploads/2017/02/Capture-d%E2%80%99e%CC%81cran-2017-02-15-a%CC%80-22.04.47.png" class="d-block w-100" alt="">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselInterval" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
+    <span class="sr-only">Précédent</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselInterval" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
+    <span class="sr-only">Suivant</span>
   </a>
 </div>
 
