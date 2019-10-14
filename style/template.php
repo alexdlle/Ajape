c6bdd16081f48d4a04d2c7cf004cf5492d3a862e
Hugo <h.deschamps@stjosup.com>
2019-10-14 10:52:39 -0700

    bhj

--- style/template.php
+++ style/template.php
@@ -12,7 +12,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="collapse navbar-collapse">
       <a class="navbar-brand" href="#" style="font-family: 'Skranji', cursive;">Ajape</a>
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0 ">
+      <ul class="navbar-nav my-2 my-lg-0 ">
         <li class="nav-item">
           <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
         </li>
