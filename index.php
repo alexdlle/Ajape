e87e7e57d85ecfd4270ecaa051a2cdfbea82c1f9
Hugo <h.deschamps@stjosup.com>
2019-10-14 11:28:40 -0700

    Carousel View

--- index.php
+++ index.php
@@ -5,12 +5,31 @@ ob_start();
 
 ?>
 
-<p>test</p>
-<a href="index.php">Link</a>
+<div id="carouselInterval" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-interval="10000">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-interval="2000">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Précédent</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Suivant</span>
+  </a>
+</div>
 
 <?php
 
 $body = ob_get_clean();
-require('./style/template.php'); 
+require('./style/template.php');
 
 ?>
