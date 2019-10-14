9e3cb967fb9b884438a1732239b74ec7f05ed071
Hugo <h.deschamps@stjosup.com>
2019-10-14 11:35:31 -0700

    carousel

--- index.php
+++ index.php
@@ -5,25 +5,25 @@ ob_start();
 
 ?>
 
-<div id="carouselInterval" class="carousel slide" data-ride="carousel">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active" data-interval="10000">
+    <div class="carousel-item active">
       <img src="..." class="d-block w-100" alt="...">
     </div>
-    <div class="carousel-item" data-interval="2000">
+    <div class="carousel-item">
       <img src="..." class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
       <img src="..." class="d-block w-100" alt="...">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselInterval" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Précédent</span>
+    <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselInterval" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Suivant</span>
+    <span class="sr-only">Next</span>
   </a>
 </div>
 
