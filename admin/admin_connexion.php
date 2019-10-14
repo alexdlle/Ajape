4d23898c1f10e75ad9ce6deeb2be97aeb9a23dc4
Alexandre D <alexandre.dessolle@gmail.com>
2019-10-14 11:11:40 +0200

    admin connexion

---
+++ admin/admin_connexion.php
@@ -0,0 +1,13 @@
+<?php
+$user = "id11179498_hugzrr";
+$pass = "cqfdcqfd";
+$dbh = new PDO('mysql:host=localhost;dbname=id11179498_ajape', $user, $pass);
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$connexionAdmin = $dbh->query('SELECT * FROM admin_users WHERE username = "'. $username .'" AND password = MD5("'. $password .'")');
+
+if(count($connexionAdmin) > 0) {
+  echo 'coucou pute';
+}
