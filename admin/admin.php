2e0fafdf18c97c474d36f39e97b89351a0768dc3
Alexandre D <alexandre.dessolle@gmail.com>
2019-10-14 10:23:06 +0200

    last admin

---
+++ admin/admin.php
@@ -0,0 +1,26 @@
+<?php
+  $title = "Administration";
+
+  ob_start();
+
+?>
+
+<form aciton="" method="post">
+  <div class="form-group col-md-6">
+    <label for="formUsername">Username</label>
+    <input type="text" name="username" id="formUsername">
+  </div>
+  <div class="form-group col-md-6">
+    <label for="formPassword">Password</label>
+    <input type="text" name="password" id="formPassword">
+  </div>
+  <button type="submit" class="btn btn-dark">Sign in</button>
+</form>
+
+
+<?php
+
+$body = ob_get_clean();
+require('../style/template.php');
+
+?>
