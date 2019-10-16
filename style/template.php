a0661a5165a4080d0100b3f461f5e931e8e6fa5e
Hugo <h.deschamps@stjosup.com>
2019-10-16 12:33:24 -0700

    s

--- style/template.php
+++ style/template.php
@@ -52,15 +52,14 @@
 
     <?= $body ?>
 
-
-    <footer class="py-5 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">Copyright © Ajape 2019</p>
-    </div>
-    <!-- /.container -->
-  </footer>
-
   </body>
+
+  <footer class="py-5 bg-dark">
+  <div class="container">
+    <p class="m-0 text-center text-white">Copyright © Ajape 2019</p>
+  </div>
+  <!-- /.container -->
+</footer>
 
 
 
