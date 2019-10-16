3daa83f7bb6ceeeada5d9b6f3616230b81361164
Hugo <h.deschamps@stjosup.com>
2019-10-16 12:46:37 -0700

    gt

--- style/template.php
+++ style/template.php
@@ -55,7 +55,7 @@
 
   </body>
 
-  <footer class="py-5 bg-dark">
+  <footer class="py-5 bg-dark footer-mod">
   <div class="container">
     <p class="m-0 text-center text-white">Copyright © Ajape 2019</p>
   </div>
