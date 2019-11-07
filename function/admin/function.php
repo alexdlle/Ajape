4fa66561dd3b0a582927bb4608d378335faad305
HugZRR <56398804+HugZRR@users.noreply.github.com>
2019-11-07 09:36:34 +0100

    Admin

---
+++ function/admin/function.php
@@ -0,0 +1,12 @@
+<?php
+
+require "./function/connection.php";
+
+class Admin extends Connection
+{
+    public function Connection() 
+    {
+        echo $test;
+    }
+}
+
---
+++ function/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+class Connection {
+    public function DbConnect()
+    {
+        $bdd = new PDO ("mysql:host=localhost; dbname=id11179498_ajape; charset=utf8','id11179498_hugzrr', 'cqfdcqfd'");
+        $test ='hello';
+    }
+}
