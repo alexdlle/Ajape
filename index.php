8914a1a3bd9b2e139b652a2457e8bf9a1e5e9ba8
HugZRR <56398804+HugZRR@users.noreply.github.com>
2019-11-07 11:49:03 +0100

    df

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ ob_start();
 <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active" data-interval="10000">
-      <img src="https://blog.sportacus.fr/wp-content/uploads/2017/02/Capture-d%E2%80%99e%CC%81cran-2017-02-15-a%CC%80-22.04.47.png" class="d-block w-100" alt="">
+      <img src="style/Images/air_jordan_1.jpg" class="d-block w-100" alt="">
     </div>
     <div class="carousel-item" data-interval="2000">
       <img src="https://cdn.pixabay.com/photo/2016/02/03/08/34/banner-1176681_960_720.jpg" class="d-block w-100" alt="">
